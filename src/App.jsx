b49153f32a28b9d505d39a0183696ddde31229d1
jgrdt0x <julien.gerardot@hotmail.com>
2024-10-25 14:52:41 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -53,31 +53,141 @@ function App() {
 
   return (
     <>
-      <div className='flex items-center justify-center h-screen'>
-        <span className='w-1/2 md:w-1/3 text-left text-[#000] text-[15px]'>
-          🗣️<br/>
+      <div className='flex flex-col items-center justify-center relative'>
+        <div className='text-left text-[#000] text-[15px] p-10 w-full md:w-[600px]'>
 
-      <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.ing/" target="_blank" rel="noopener noreferrer">yap.ing</a> about what <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.is/" target="_blank" rel="noopener noreferrer">yap.is</a> will soon get you <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.money/" target="_blank" rel="noopener noreferrer">yap.money</a> so you can have <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.cash/" target="_blank" rel="noopener noreferrer">yap.cash</a> to spend <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.shopping/" target="_blank" rel="noopener noreferrer">yap.shopping</a> or at the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.store/" target="_blank" rel="noopener noreferrer">yapple.store</a> or use <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.link/" target="_blank" rel="noopener noreferrer">yappay.link</a> to pay for <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.plus/" target="_blank" rel="noopener noreferrer">yap.plus</a> so you can have <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.news/" target="_blank" rel="noopener noreferrer">yap.news</a> & <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.codes/" target="_blank" rel="noopener noreferrer">yap.codes</a> for incentivized referrals in the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.community/" target="_blank" rel="noopener noreferrer">yap.community</a> or a premium <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.page/" target="_blank" rel="noopener noreferrer">yap.page</a> or to listen to <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.zip/" target="_blank" rel="noopener noreferrer">yap.zip</a> we just tryna <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.it.com/" target="_blank" rel="noopener noreferrer">yap.it.com</a> to the moon so we can <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.farm/" target="_blank" rel="noopener noreferrer">yap.farm</a> on <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.earth/" target="_blank" rel="noopener noreferrer">yap.earth</a> but that is just the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.today/" target="_blank" rel="noopener noreferrer">yap.today</a> on the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.site/" target="_blank" rel="noopener noreferrer">yap.site</a> welcome to the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.team/" target="_blank" rel="noopener noreferrer">yap.team</a> of the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline" href="https://www.yap.meme/" target="_blank" rel="noopener noreferrer">yap.meme</a>  
+          <div className='text-left border-b border-[#0000FF] mb-4'>
+            <div className='flex justify-between'> 
+              <h2 className=' italic font-bold '>Index of/</h2>
+              <span className='text-right text-white bg-[#0000FF]'>buy $yap</span>
+            </div>
+          </div>
+          🗣️🗣️🗣️
 
 
-      <br/><br/>
-      
-          <span className='text-[11px] underline' onClick={handleCopy} style={{ cursor: 'pointer' }}>
-            3gcdoCBQMKAFVopsQ377X7JhkcNPg9fwPY4eW46Apump
-          </span>
-          {toastVisible && <div className="toast text-[#000be2] text-[11px] ">yaped in clipboard</div>}
+          <div className='spacer'></div>
+
+          <div>
+          <h2 className='border-b border-[#0000FF] italic mb-4'>welcome/yaptivators/</h2>
+          <p>We <i>yappers</i>, pushing boudaries in web3, fashion, technology through groundbreaking collaborations and community-driven innovations.</p>
+          </div>
+
+
+          <div className='spacer'></div>
+
+
+          <div>
+          <h2 className='border-b border-[#0000FF] italic mb-4'>we/yapping/</h2>
+
+          <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.ing/" target="_blank" rel="noopener noreferrer">yap.ing</a> about what <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.is/" target="_blank" rel="noopener noreferrer">yap.is</a> will soon get you <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.money/" target="_blank" rel="noopener noreferrer">yap.money</a> so you can have <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.cash/" target="_blank" rel="noopener noreferrer">yap.cash</a> to spend <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.shopping/" target="_blank" rel="noopener noreferrer">yap.shopping</a> or at the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.store/" target="_blank" rel="noopener noreferrer">yapple.store</a> or use <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.link/" target="_blank" rel="noopener noreferrer">yappay.link</a> to pay for <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.plus/" target="_blank" rel="noopener noreferrer">yap.plus</a> so you can have <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.news/" target="_blank" rel="noopener noreferrer">yap.news</a> & <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.codes/" target="_blank" rel="noopener noreferrer">yap.codes</a> for incentivized referrals in the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.community/" target="_blank" rel="noopener noreferrer">yap.community</a> or a premium <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.page/" target="_blank" rel="noopener noreferrer">yap.page</a> or to listen to <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.zip/" target="_blank" rel="noopener noreferrer">yap.zip</a> we just tryna <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.it.com/" target="_blank" rel="noopener noreferrer">yap.it.com</a> to the moon so we can <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.farm/" target="_blank" rel="noopener noreferrer">yap.farm</a> on <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.earth/" target="_blank" rel="noopener noreferrer">yap.earth</a> but that is just the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.today/" target="_blank" rel="noopener noreferrer">yap.today</a> on the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.site/" target="_blank" rel="noopener noreferrer">yap.site</a> welcome to the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.team/" target="_blank" rel="noopener noreferrer">yap.team</a> of the <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="underline font-bold" href="https://www.yap.meme/" target="_blank" rel="noopener noreferrer">yap.meme</a>  
+        <br/><br/>
+        <span className='text-[15px] bg-[#000FFF] text-white underline cursor-pointer' onClick={handleCopy}>
+          3gcdoCBQMKAFVopsQ377X7JhkcNPg9fwPY4eW46Apump
         </span>
+        {toastVisible && <div className="toast text-[#000FFF] text-[15px] italic">in clipboard</div>}
 
-        {hoveredImage && (
-          <img 
-            src={hoveredImage} 
-            alt="hovered" 
-            style={{ position: 'absolute', top: '30%', left: '30%', transform: 'translate(-50%, -50%)', pointerEvents: 'none' }} // Adjusted position
-          />
-        )}
+          {hoveredImage && (
+            <img 
+              className=' max-w-60 border border-[#0000FF]'
+              src={hoveredImage} 
+              alt="hovered" 
+              style={{ position: 'absolute', top: '200px', left: '0px', transform: 'translate(-50%, -50%)', pointerEvents: 'none' }} // Adjusted position
+            />
+          )}
+          </div>
 
-        <div className='footer fixed bottom-4 text-[10px] text-gray-400'>Yap Site 2024 ©</div>
+
+
+
+          <div className='spacer'></div>
+
+
+
+
+
+          <div>
+            <h2 className='border-b border-[#0000FF] italic mb-4'>yap/build/cook</h2>
+            
+            <div className='flex flex-wrap items-center gap-3'>
+              <img src="/logo/yapcapital.png" alt="yapcorp" className="h-24" />
+              <img src="/logo/yapplestore.png" alt="yapcorp" className="h-24" />
+              <img src="/logo/yappay.png" alt="yapcorp" className="h-24" />
+              <img src="/logo/yapcapital.png" alt="yapcorp" className="h-24" />
+              <img src="/logo/yapplestore.png" alt="yapcorp" className="h-24" />
+              <img src="/logo/yapcapital.png" alt="yapcorp" className="h-24" />
+              <img src="/logo/yapplestore.png" alt="yapcorp" className="h-24" />
+              <img src="/logo/yappay.png" alt="yapcorp" className="h-24" />
+            </div>
+          </div>
+
+
+
+          <div className='spacer'></div>
+
+
+
+
+          <div>
+          <h2 className='border-b border-[#0000FF] italic mb-4'>we/yapping/</h2>
+          <p>A collective of visionaries shaping the future of digital experiences and decentralized technologies.</p>
+          </div>
+          
+          <div className='team-profiles flex space-x-4 mt-4'>
+            {['#f0da91', '#f0da91', '#f0da91', '#f0da91', '#f0da91'].map((color, index) => (
+              <div key={index} className='flex flex-col items-center'>
+                <div className=' border rounded-full p-1 border-[#0000FF]'>
+                <div className='w-8 h-8 rounded-full' style={{ backgroundColor: color }}></div>
+                </div>
+                <span className='text-xs text-[#0000FF]'>{color}</span>
+              </div>
+            ))}
+          </div>
+
+
+
+
+          <div className='spacer'></div>
+
+
+          <div>
+          <h2 className='border-b border-[#0000FF] italic mb-4'>worked/working/with/</h2>
+
+          <div className='brand-logos flex items-center space-x-4'>
+          {['/logo/1.png', '/logo/2.png', '/logo/3.png', '/logo/4.png', '/logo/5.png'].map((logo, index) => (
+            <div key={index} className='flex items-center'>
+              <img src={logo} alt={`Brand logo ${index + 1}`} className='w-auto h-auto' />
+            </div>
+          ))}
+        </div>
+
+          </div>
+
+
+          <div className='spacer'></div>
+
+          <div>
+          <h2 className='border-b border-[#0000FF] italic mb-4'>communities/yap/contribute/</h2>
+
+          <div className='brand-logos flex space-x-4'>
+
+          <span className='text-right text-white bg-[#0000FF]'>$wif</span>
+          <span className='text-right text-white bg-[#0000FF]'>$sfc</span>
+          <span className='text-right text-white bg-[#0000FF]'>$mew</span>
+          <span className='text-right text-white bg-[#0000FF]'>$michi</span>
+
+
+          </div>
+
+          </div>
+
+
       </div>
+
+      <div className='spacer'></div>
+      <div className='spacer'></div>
+      <div className='footer fixed bottom-4 text-[10px] text-[#0000FF] cursor-wait'>yap site 2024 ©</div>
+      </div>
+
     </>
   )
 }
